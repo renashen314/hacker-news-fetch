59a70120922d300ba64a3bc5d394d2e0d937c3b0
Rena Shen <renashen314@hotmail.com>
2026-03-23 20:28:02 -0400

    fetch top 10 stories on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+
+type Story = {
+  score: string;
+  title: string;
+  url: string;
+  by: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url =
+    "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+
+  const fetchIds = async (url: string) => {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json.slice(0, 10);
+  };
+
+  const fetchStories = async (ids: string[]) => {
+    try {
+      const promises = ids.map(async (id) => {
+        const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
+        return fetch(url).then((r) => r.json());
+      });
+      const data = await Promise.all(promises);
+      const stories = data.map((d) => {
+        return {
+          score: d.score,
+          title: d.title,
+          url: d.url,
+          by: d.by,
+        };
+      });
+      return stories;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const [stories, setStories] = useState<Story[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const ids = await fetchIds(url);
+      const data = await fetchStories(ids);
+      setStories(data);
+    };
+    load();
+  }, []);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div>
+      <ul>
+        {stories.map((s, i) => (
+          <li key={i}>
+            <a href={s.url}>{s.title}</a>
+            <p>
+              {s.score} by {s.by}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
